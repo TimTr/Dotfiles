20fff377c5e6ac40c9e7be2a37a44d34ade1b51f
Tim Triemstra <root@timtr.com>
2021-01-09 13:42:15 -0800

    Started adding support for setup of VS Code

--- macos-vscode/update-vscode.sh
+++ macos-vscode/update-vscode.sh
@@ -9,5 +9,15 @@ status "update-vscode.sh - start" "Configuring Visual Studio Code settings"
 
 ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" ${DEVELOPER_BIN}/vscode 2> /dev/null
 
+# Copy app preferences
+cp -R ${DOTFILES_ROOT}/macos-vscode/vscode-prefs/* $HOME/Library/Preferences
+
+# Copy extensions and other settings to ~/.vscode (first clean it out)
+mkdir ${HOME}/.vscode &> /dev/null
+rm -Rf ${HOME}/.vscode/* &> /dev/null
+cp ${DOTFILES_ROOT}/macos-vscode/vscode-extensions.zip ${HOME}/.vscode/
+
+
+
 # ==============================================================================
 exit 0
