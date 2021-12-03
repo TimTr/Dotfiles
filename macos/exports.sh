6a635cc60e466be5159fecf8e12ec6cf2300f359
Fill MeIn <example@example.com>
2021-12-02 16:03:51 -0800

    Updated Xcode and Terminal themes. Moved some Xcode defaults into the exports.sh file instead of the functions.sh to better organize.

--- macos/exports.sh
+++ macos/exports.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# Exports environment variables, but also sets `defaults` for the system
 # TODO: haven't yet actually done the work to make this file good!!!
 
 # ==============================================================================
@@ -17,6 +18,41 @@ export LSCOLORS=gxFxCxDxbxExBxAxaxaxex
 
 # Make VSCode the default editor.
 export EDITOR='code';
+
+# Some cool Xcode defaults:  https://github.com/ctreffs/xcode-defaults
+
+# Tells Xcode to never re-open last open project, regardless of OS setting
+defaults write com.apple.dt.Xcode ApplePersistenceIgnoreState -bool YES
+
+# This one stops the IDE from saving the workspace layout
+# Probably don't want this one enabled
+# defaults write com.apple.dt.Xcode IDEDisableStateRestoration -bool YES
+defaults delete com.apple.dt.Xcode IDEDisableStateRestoration
+
+# Sets Xcode to show extensions for .sh and .md files, additionally
+# NOTE: the single-quote, comma + space format, with no space before/after the
+# paranthesis must be precise
+defaults write com.apple.dt.Xcode IDEFileExtensionDisplayShowOnlyList '(c, cc, cpp, h, hpp, m, mm, gif, icns, jpeg, jpg, png, tiff, sh, md)'
+
+# Save screenshots to the downloads folder
+defaults write com.apple.screencapture location -string “$HOME/Downloads”
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string “png”
+
+# Removes the delay in hide/show the Dock setting
+# defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
+
+# Display full POSIX path as Finder window title
+# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Finder: show hidden files by default
+# defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Automatically hide and show the Dock
+# defaults write com.apple.dock autohide -bool true
+
+
 
 # Tell grep to highlight matches (throwing errors)
 # export GREP_OPTIONS='—color=auto'
--- macos/functions.sh
+++ macos/functions.sh
@@ -42,37 +42,10 @@ install-github-token() {
     echo "export HOMEBREW_GITHUB_API_TOKEN=$1" >> ~/.zshrc
 }
 
+
 # Delete Xcode derived data
 delete-derived-data() {
     echo "Deleting all Xcode derived data..."
     rm -rdf ~/Library/Developer/Xcode/DerivedData/*
 }
 
-# Some cool Xcode defaults:  https://github.com/ctreffs/xcode-defaults
-
-# Tells Xcode to never re-open last open project, regardless of OS setting
-defaults write com.apple.dt.Xcode ApplePersistenceIgnoreState -bool YES
-defaults write com.apple.dt.Xcode IDEDisableStateRestoration -bool YES
-
-# Sets Xcode to show extensions for .sh and .md files, additionally
-# NOTE: the single-quote, comma + space format, with no space before/after the
-# paranthesis must be precise
-defaults write com.apple.dt.Xcode IDEFileExtensionDisplayShowOnlyList '(c, cc, cpp, h, hpp, m, mm, gif, icns, jpeg, jpg, png, tiff, sh, md)'
-
-# Save screenshots to the downloads folder
-defaults write com.apple.screencapture location -string “$HOME/Downloads”
-
-# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-defaults write com.apple.screencapture type -string “png”
-
-# Removes the delay in hide/show the Dock setting
-# defaults write com.apple.Dock autohide-delay -float 0 && killall Dock
-
-# Display full POSIX path as Finder window title
-# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
-# Finder: show hidden files by default
-# defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Automatically hide and show the Dock
-# defaults write com.apple.dock autohide -bool true
