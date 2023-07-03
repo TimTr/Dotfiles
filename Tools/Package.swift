b50cf3f21c537cbdd8a6dbb4e106877e0c8e0ce5
Tim Triemstra <root@timtr.com>
2023-07-02 20:40:08 -0700

    Refactored to put the Swift tool into a subfolder ./Tools

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -9,8 +9,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+
         .executableTarget(
             name: "SamplePackage",
             dependencies: [
