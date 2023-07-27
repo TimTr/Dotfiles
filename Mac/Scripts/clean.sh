7f3566ab368efa5e001fe900a67d84c9d8570b6f
Tim Triemstra <root@timtr.com>
2023-07-26 17:04:18 -0700

    Updated Terminal colors, removed the Swift package (that shouild be a different repo) and added a new "clean.sh" command to scripts.

---
+++ Mac/Scripts/clean.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+#
+# clean.sh   -- simple instructions on how to use these dotfiles
+#
+# ==============================================================================
+
+# Import the default functions used in these scripts
+source ${HOME}/.functions
+message "clean.sh" "Reset caches and preferences to default"
+# ==============================================================================
+
+
+
+
+# Hide the ~/Documents/Insta360/ folder from Finder, if present
+chflags hidden ${HOME}/Documents/Insta360/   &> /dev/null
+
+
+
